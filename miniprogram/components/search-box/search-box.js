5dbe344b06add3ad8520ca3d640be0d393f5a0c7
suyu <halfxianhuman@hotmail.com>
2021-11-01 16:39:15 +0800

    search-box of home

---
+++ miniprogram/components/search-box/search-box.js
@@ -0,0 +1,69 @@
+// components/search-box/search-box.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder:{
+      type: String,
+      value:''
+    },
+    middle: {
+      type: Boolean,
+      value: true
+    },
+    inputValue: ''
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showCancel: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    inputFocus(e){
+      this.setData({
+        middle: false
+      })
+    },
+    inputBlur(e){
+      if(!e.detail.value){
+        this.setData({
+          middle: true
+        })
+      }
+    },
+    inputHandler(e){
+      const {value} = e.detail
+      const { showCancel } = this.data
+      if(!value) {
+        this.setData({
+          showCancel: false
+        })
+        return
+      }
+      if(!showCancel){
+        this.setData({
+          showCancel:true
+        })
+      }
+    },
+    clearInput(e){
+      this.setData({
+        inputValue: '',
+        showCancel: false,
+        middle: true
+      })
+    },
+    submit(e){
+      this.triggerEvent('searchevent',{
+        keyword: this.data.inputValue
+      })
+    }
+  }
+})
